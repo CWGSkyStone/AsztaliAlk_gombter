5d855703925b1978ddff95f77cda8da9f0edb854
Szabo Jozsef <skystone213@gmail.com>
2026-04-20 13:38:46 +0200

    Kezdetlesges UI

---
+++ src/main/java/dev/skystone/MainController.java
@@ -0,0 +1,23 @@
+package dev.skystone;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class MainController {
+
+    @FXML
+    private TextField radiusField;
+
+    @FXML
+    private TextField surfaceField;
+
+    @FXML
+    private TextField volumeField;
+
+    @FXML
+    void onClickCalcButton(ActionEvent event) {
+
+    }
+
+}
