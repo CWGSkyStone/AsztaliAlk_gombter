422646f022ddd00d4aa0b6732acc6550fb09a56f
Szabo Jozsef <skystone213@gmail.com>
2026-04-20 14:55:33 +0200

    tryWriteToFile metódus

--- src/main/java/dev/skystone/FileHandler.java
+++ src/main/java/dev/skystone/FileHandler.java
@@ -2,16 +2,21 @@ package dev.skystone;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 public class FileHandler {
     static void writeToFile(String line){
-        try (FileWriter writer = new FileWriter("results.txt", true)){
-            writer.write(line);
+        try{
+            tryWirteToFile(line);
         }
         catch (IOException e){           
             System.err.println(e.getMessage());
         }
     }
-    
+    static void tryWirteToFile(String line) throws IOException{
+        FileWriter wr = new FileWriter("results.txt", Charset.forName("UTF-8"), true);
+        wr.write(line);
+        wr.close();
+    }
 
 }
